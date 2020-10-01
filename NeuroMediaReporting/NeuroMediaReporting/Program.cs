05497a1f8c5dbae147a811b6f2fb4a106b01d506
brandon-lambert <brandon-lambert@hotmail.com>
2020-10-01 14:45:19 +0200

    Add Solution files

---
+++ NeuroMediaReporting/NeuroMediaReporting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeuroMediaReporting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
