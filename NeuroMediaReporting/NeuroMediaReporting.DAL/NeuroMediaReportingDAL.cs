50a284b6e8f27c6dc979b60b3749c6a280ebc488
brandon-lambert <brandon-lambert@hotmail.com>
2020-10-01 15:49:13 +0200

    Implement ReadFile in DAL

--- NeuroMediaReporting/NeuroMediaReporting.DAL/NeuroMediaReportingDAL.cs
+++ NeuroMediaReporting/NeuroMediaReporting.DAL/NeuroMediaReportingDAL.cs
@@ -1,9 +1,53 @@
-﻿using System;
+﻿using NeuroMediaReporting.DAL.Entities;
+using SoftCircuits.CsvParser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace NeuroMediaReporting.DAL
 {
     public class NeuroMediaReportingDAL
     {
-       
+       public IEnumerable<SessionReportDAO> SearchSessionReports(string dirPath, string mask)
+        {
+            List<SessionReportDAO> sessionReports = new List<SessionReportDAO>();
+
+            foreach (var filePath in GetFileList(dirPath, mask))
+            {
+                GetAllReportFromFile(filePath);
+            }
+
+            return null;
+        }
+
+        public IEnumerable<SessionReportDAO> GetAllReportFromFile(string filePath)
+        {
+            List<SessionReportDAO> sessionReports = new List<SessionReportDAO>();
+            using (CsvReader<SessionReportDAO> reader = new CsvReader<SessionReportDAO>(filePath))
+            {
+                // Read header and use to determine column order
+                reader.ReadHeaders(false);
+                // Read data
+                while (reader.Read(out SessionReportDAO sessionReportDAO))
+                    sessionReports.Add(sessionReportDAO);
+            }
+
+            return sessionReports;
+        }
+
+        public IEnumerable<string> GetFileList(string dirPath, string mask)
+        {
+            var allFilesPaths = Directory.GetFiles(dirPath);
+
+            return allFilesPaths.Where(fp => FitsMask(Path.GetFileName(fp), mask));
+        }
+
+        private bool FitsMask(string sFileName, string sFileMask)
+        {
+            Regex mask = new Regex(sFileMask.Replace(".", "[.]").Replace("*", ".*").Replace("?", "."));
+            return mask.IsMatch(sFileName);
+        }
     }
 }
--- NeuroMediaReporting/Testing/NeuroMediaReporting.DAL.UnitTest/DALUnitTest.cs
+++ NeuroMediaReporting/Testing/NeuroMediaReporting.DAL.UnitTest/DALUnitTest.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NeuroMediaReporting.DAL.Entities;
 using SoftCircuits.CsvParser;
 using System.Collections.Generic;
+using System.IO;
 
 namespace NeuroMediaReporting.DAL.UnitTest
 {
@@ -12,7 +13,7 @@ namespace NeuroMediaReporting.DAL.UnitTest
         public void ReadFile_Test()
         {
             List<SessionReportDAO> people = new List<SessionReportDAO>();
-            using (CsvReader<SessionReportDAO> reader = new CsvReader<SessionReportDAO>(@"C:\NeuroMediaTest\Logs\access"))
+            using (CsvReader<SessionReportDAO> reader = new CsvReader<SessionReportDAO>(@"access.log"))
             {
                 // Read header and use to determine column order
                 reader.ReadHeaders(false);
